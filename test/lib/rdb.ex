deecc0442d58be2b33d0a52a021a4efdfd57c0a3
sdancer <ktion23@gmail.com>
2025-07-05 22:44:26 -0300

    compiles

--- test/lib/rdb.ex
+++ test/lib/rdb.ex
@@ -1,15 +1,9 @@
 defmodule RustlerRocksDB do
-
   use Rustler, otp_app: :rustler_rocksdb, crate: "rustler_rocksdb"
 
+  def init(_db_path), do: :erlang.nif_error(:nif_not_loaded)
 
+  def get(_key), do: :erlang.nif_error(:nif_not_loaded)
 
-    # When your NIF is loaded, it will override these functions
-
-      def init(_db_path), do: :erlang.nif_error(:nif_not_loaded)
-
-        def get(_key), do: :erlang.nif_error(:nif_not_loaded)
-
-          def put(_key, _value), do: :erlang.nif_error(:nif_not_loaded)
-
-          end
+  def put(_key, _value), do: :erlang.nif_error(:nif_not_loaded)
+end
--- test/native/rustler_rocksdb/src/lib.rs
+++ test/native/rustler_rocksdb/src/lib.rs
@@ -1,14 +1,19 @@
-// Rust implementation in native/rustler_rocksdb/src/lib.rs
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
 use rustler::{Encoder, Env, NifResult, Term};
-
-use rocksdb::{Options, DB};
+use rustler::{Error};
 
 use std::sync::Mutex;
 
 use lazy_static::lazy_static;
 
 use std::path::Path;
+
+use rustler::Binary;
+use rustler::OwnedBinary;
+
+use rocksdb::{Options, DB};
 
 lazy_static! {
     static ref DB_INSTANCE: Mutex<Option<DB>> = Mutex::new(None);
@@ -103,97 +108,94 @@ fn put(key: String, value: Vec<u8>) -> NifResult<bool> {
     }
 }
 
-// ---------- NIF skeletons with `env` ----------
-use rustler::{Env, Term, NifResult, Error};  // ← added Error
-
 // ------------------------ NIF skeletons ------------------------
 
 #[rustler::nif(name = "transaction_get_3")]
-fn transaction_get_3(_env: Env, _txn_id: String, _key: String, _opts: Term)
+fn transaction_get_3(_txn_id: String, _key: String, _opts: Term)
     -> NifResult<Option<Vec<u8>>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "transaction_get_4")]
-fn transaction_get_4(_env: Env, _txn_id: String, _key: String, _opts: Term, _cf: String)
+fn transaction_get_4(_txn_id: String, _key: String, _opts: Term, _cf: String)
     -> NifResult<Option<Vec<u8>>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "get_3")]
-fn get_3(_env: Env, _key: String, _opts: Term)
+fn get_3(_key: String, _opts: Term)
     -> NifResult<Option<Vec<u8>>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "get_4")]
-fn get_4(_env: Env, _key: String, _opts: Term, _cf: String)
+fn get_4(_key: String, _opts: Term, _cf: String)
     -> NifResult<Option<Vec<u8>>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "iterator_move_2")]
-fn iterator_move_2(_env: Env, _iterator: Term, _direction: Term)
+fn iterator_move_2(_iterator: Term, _direction: Term)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "transaction_put_3")]
-fn transaction_put_3(_env: Env, _txn_id: String, _key: String, _value: Vec<u8>)
+fn transaction_put_3(_txn_id: String, _key: String, _value: Vec<u8>)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "transaction_put_4")]
-fn transaction_put_4(_env: Env, _txn_id: String, _key: String, _value: Vec<u8>, _opts: Term)
+fn transaction_put_4(_txn_id: String, _key: String, _value: Vec<u8>, _opts: Term)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "put_4")]
-fn put_4(_env: Env, _key: String, _value: Vec<u8>, _opts: Term)
+fn put_4(_key: String, _value: Vec<u8>, _opts: Term)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "put_5")]
-fn put_5(_env: Env, _key: String, _value: Vec<u8>, _opts: Term, _cf: String)
+fn put_5(_key: String, _value: Vec<u8>, _opts: Term, _cf: String)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "transaction_delete_2")]
-fn transaction_delete_2(_env: Env, _txn_id: String, _key: String)
+fn transaction_delete_2(_txn_id: String, _key: String)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "transaction_delete_3")]
-fn transaction_delete_3(_env: Env, _txn_id: String, _key: String, _opts: Term)
+fn transaction_delete_3(_txn_id: String, _key: String, _opts: Term)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "delete_3")]
-fn delete_3(_env: Env, _key: String, _opts: Term)
+fn delete_3(_key: String, _opts: Term)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "delete_4")]
-fn delete_4(_env: Env, _key: String, _opts: Term, _cf: String)
+fn delete_4(_key: String, _opts: Term, _cf: String)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
@@ -207,64 +209,41 @@ fn transaction_iterator_2(env: Env, _txn_id: String)
 }
 
 #[rustler::nif(name = "transaction_iterator_3")]
-fn transaction_iterator_3(env: Env, _txn_id: String, _opts: Term)
-    -> NifResult<Term>
+fn transaction_iterator_3<'a>(env: Env<'a>, _txn_id: String, _opts: Term<'a>)
+    -> NifResult<Term<'a>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "iterator_2")]
-fn iterator_2(env: Env, _opts: Term)
-    -> NifResult<Term>
+fn iterator_2<'a>(env: Env<'a>, _opts: Term<'a>)
+     -> NifResult<Term<'a>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "iterator_3")]
-fn iterator_3(env: Env, _opts: Term, _cf: String)
-    -> NifResult<Term>
+fn iterator_3<'a>(env: Env<'a>, _opts: Term<'a>, _cf: String)
+     -> NifResult<Term<'a>>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "flush_3")]
-fn flush_3(_env: Env, _opts: Term, _wait: bool)
+fn flush_3(_opts: Term, _wait: bool)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 #[rustler::nif(name = "compact_range_5")]
-fn compact_range_5(_env: Env, _start: Term, _end: Term, _opts: Term, _cf: String, _output_level: i32)
+fn compact_range_5(_start: Term, _end: Term, _opts: Term, _cf: String, _output_level: i32)
     -> NifResult<bool>
 {
     Err(Error::Atom("not_implemented"))
 }
 
 // ---------- Rustler exports ----------
-rustler::init!("RustlerRocksDB", [
-    init,
-    get,
-    put,
-    transaction_get_3,
-    transaction_get_4,
-    get_3,
-    get_4,
-    iterator_move_2,
-    transaction_put_3,
-    transaction_put_4,
-    put_4,
-    put_5,
-    transaction_delete_2,
-    transaction_delete_3,
-    delete_3,
-    delete_4,
-    transaction_iterator_2,
-    transaction_iterator_3,
-    iterator_2,
-    iterator_3,
-    flush_3,
-    compact_range_5
-]);
+rustler::init!("Elixir.RustlerRocksDB");
 
 
